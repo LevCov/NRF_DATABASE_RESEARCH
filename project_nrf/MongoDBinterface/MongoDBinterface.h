a5b56233ec0bb61a90c6ea657ba35f6eeb889e45
LevCov <100800333+LevCov@users.noreply.github.com>
2023-10-25 13:37:10 +0300

    Update MongoDBinterface.h
    Signed-off-by: LevCov <100800333+LevCov@users.noreply.github.com>

--- project_nrf/MongoDBinterface/MongoDBinterface.h
+++ project_nrf/MongoDBinterface/MongoDBinterface.h
@@ -1,1 +1,73 @@
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <mongocxx/instance.hpp>
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/uri.hpp>
+#include <bsoncxx/json.hpp>
+#include <string>
+#include <typeinfo>
+class MongoDBConnector {
+public:
+    MongoDBConnector(const char *uri_string) {
+        
+        client_ = mongocxx::client{ mongocxx::uri{uri_string} };
+        
+    };
+    
+
+    void InsertDocument(const char* database_name, const char* collection_name, const bsoncxx::document::value& document) {
+        
+        
+        mongocxx::client client_(uri);
+
+        auto db = client_[database_name];
+        auto collection = db[collection_name];
+        collection.insert_one(document.view());
+    }
+
+    auto FindDocuments (const char* database_name, const char* collection_name, const bsoncxx::document::view_or_value& filter) {
+        
+        
+        mongocxx::client client_(uri);
+
+        auto db = client_[database_name];
+        auto collection = db[collection_name];
+        return collection.find(filter);
+
+        
+    }
+
+    void UpdateDocument(const char* database_name, const char* collection_name, const bsoncxx::document::view_or_value& filter, const bsoncxx::document::view_or_value& update) {
+       
+        mongocxx::client client_(uri);
+        
+        auto db = client_[database_name];
+        auto collection = db[collection_name];
+        collection.update_one(filter.view(), update.view());
+    }
+
+    void DeleteDocument(const char* database_name, const char* collection_name, const bsoncxx::document::view_or_value& filter) {
+        // Инициализация MongoDB
+        mongocxx::client client_(uri);
+        
+        auto db = client_[database_name];
+        auto collection = db[collection_name];
+        collection.delete_one(filter);
+    }
+
+
+
+
+
+private:
+    mongocxx::client client_;
+    mongocxx::uri uri;
+    std::string db;
+    std::string collection;
+};
+
+
 
