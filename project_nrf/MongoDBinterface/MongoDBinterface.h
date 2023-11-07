a9b910f906e39ef9815c379d64205022bd4a0e0e
LevCov <100800333+LevCov@users.noreply.github.com>
2023-11-07 20:25:02 +0300

    Update MongoDBinterface.h
    Signed-off-by: LevCov <100800333+LevCov@users.noreply.github.com>

--- project_nrf/MongoDBinterface/MongoDBinterface.h
+++ project_nrf/MongoDBinterface/MongoDBinterface.h
@@ -1,57 +1,85 @@
-#include <iostream>
-#include <stdio.h>
-#include <fstream>
-#include <mongocxx/instance.hpp>
-#include <bsoncxx/builder/basic/document.hpp>
-#include <bsoncxx/builder/stream/document.hpp>
-#include <mongocxx/client.hpp>
-#include <mongocxx/uri.hpp>
-#include <bsoncxx/json.hpp>
-#include <string>
-#include <typeinfo>
+
+#include <iostream> 
+#include <fstream> 
+#include <mongocxx/client.hpp> 
+#include <mongocxx/instance.hpp> 
+#include <bsoncxx/builder/basic/document.hpp> 
+#include <bsoncxx/builder/stream/document.hpp> 
+#include <mongocxx/bulk_write.hpp>
+#include <mongocxx/uri.hpp> 
+#include <bsoncxx/json.hpp> 
+#include <bsoncxx/types.hpp>
+#include <bsoncxx/stdx/make_unique.hpp>
+#include <bsoncxx/stdx/optional.hpp>
+#include <bsoncxx/document/view_or_value.hpp>
+
+#include <string> 
+ 
 
 class MongoDBConnector {
 public:
-    MongoDBConnector(const char *uri_string) {
- 
+    MongoDBConnector(const char* uri_string) {
+
         client_ = mongocxx::client{ mongocxx::uri{uri_string} };
-        
+
     };
-    
+
+
+    void Create(const char* database_name, const char* collection_name) {
+
+        mongocxx::instance instance{};
+
+        try {
+
+            client_ = mongocxx::client{ mongocxx::uri{"mongodb://localhost:27017"} };
+            mongocxx::database db = client_[database_name];
+
+            // Создаем коллекцию 
+            mongocxx::collection coll = db[collection_name];
+
+            bsoncxx::document::value doc_value = bsoncxx::from_json("{\"key\": \"value\"}");
+            coll.insert_one(doc_value.view());
+            std::cout << std::endl<<"DB created!" << std::endl;
+        }
+        catch (const std::exception& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+
+    }
+
+    void Drop(const char* database_name)
+    {
+        mongocxx::instance instance{};
+
+        client_[database_name].drop();
+ 
+   
+    }
+
 
     void InsertDocument(const char* database_name, const char* collection_name, const bsoncxx::document::value& document) {
-        
+
         mongocxx::instance instance{};
         auto db = client_[database_name];
         auto collection = db[collection_name];
         collection.insert_one(document.view());
     }
 
-    auto FindDocuments (const char* database_name, const char* collection_name, const bsoncxx::document::view_or_value& filter) {
-        
-        
-        mongocxx::instance instance{};
-
-        auto db = client_[database_name];
-        auto collection = db[collection_name];
-        return collection.find(filter);
-
-        
-    }
-
+   
     void UpdateDocument(const char* database_name, const char* collection_name, const bsoncxx::document::view_or_value& filter, const bsoncxx::document::view_or_value& update) {
-       
+        
+        
         mongocxx::instance instance{};
         
-        auto db = client_[database_name];
+        auto db = client_[database_name]; 
         auto collection = db[collection_name];
-        collection.update_one(filter.view(), update.view());
+        collection.update_one(filter, update);
     }
 
     void DeleteDocument(const char* database_name, const char* collection_name, const bsoncxx::document::view_or_value& filter) {
-        
+
         mongocxx::instance instance{};
-        
+
         auto db = client_[database_name];
         auto collection = db[collection_name];
         collection.delete_one(filter);
@@ -67,3 +95,5 @@ private:
     std::string dbname;
     std::string colname;
 };
+
+
