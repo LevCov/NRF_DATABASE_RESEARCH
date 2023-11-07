14efb666b62e6006d2b30a3a1baab09accfe4ff4
LevCov <100800333+LevCov@users.noreply.github.com>
2023-11-07 22:42:07 +0300

    Update Test.cpp
    Signed-off-by: LevCov <100800333+LevCov@users.noreply.github.com>

--- project_nrf/MongoDBinterface/Test.cpp
+++ project_nrf/MongoDBinterface/Test.cpp
@@ -1,85 +1,110 @@
-#include "MongoDBinterface.h"
+#include "pch.h"
+#include "...\MongoDBConnector.h"
 
 
-int main() {
+class MongoDBConnectorTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        // Подготовка к тестированию, если необходимо
+    }
 
+    void TearDown() override {
+        // Очистка после тестирования, если необходимо
+    }
 
-
-    MongoDBConnector connector("mongodb://localhost:27017");
-    mongocxx::instance instance{};
-
-
-    /*
-    // Пример обновления документа
-    // Фильтр для поиска документа, который вы хотите обновить
-    bsoncxx::builder::stream::document filter;
-    filter << "key1" << "1";
-
-    // Документ с обновлениями
-    bsoncxx::builder::stream::document update;
-    update << "$set" << bsoncxx::builder::stream::open_document
-        << "key11" << "11"
-        << bsoncxx::builder::stream::close_document;
-
-
-    connector.UpdateDocument("mydb", "mycollection", filter.extract(), update.extract());
-
-
-    */
+    MongoDBConnector connector{ "mongodb://localhost:27017" };
+};
 
 
 
+TEST_F(MongoDBConnectorTest, Create) {
+    const char* database_name1 = "test_db1";
+    const char* database_name2 = "test_db2";
+    const char* collection_name = "test_collection";
 
+    connector.Create(database_name1, collection_name);
+    connector.Create(database_name2, collection_name);
 
-
-    /*
-    // Пример добавления документа
-    bsoncxx::builder::basic::document doc_builder;
-    doc_builder.append(bsoncxx::builder::basic::kvp("key1", "1"));
-    connector.InsertDocument("mydb", "mycollection", doc_builder.extract());
-
-
-    doc_builder.append(bsoncxx::builder::basic::kvp("key2", "2"));
-    connector.InsertDocument("mydb", "mycollection", doc_builder.extract());
-
-    doc_builder.append(bsoncxx::builder::basic::kvp("key3", "3"));
-    connector.InsertDocument("mydb", "mycollection", doc_builder.extract());
-
-    doc_builder.append(bsoncxx::builder::basic::kvp("key4", "4"));
-    connector.InsertDocument("mydb", "mycollection", doc_builder.extract());
-    */
-
-
-
-    /*
-     // Пример поиска документа
-     bsoncxx::builder::basic::document filter_builder;
-     filter_builder.append(bsoncxx::builder::basic::kvp("key1", "value1"));
-     bsoncxx::document::value data = filter_builder.extract();
-
-     auto a = connector.FindDocuments("mydb", "mycollection", filter_builder.view());
-
-     //std::cout << typeid(a).name() << std::endl;
-     */
-
-
-
-     /*
-
-     bsoncxx::builder::basic::document filter;
-     filter.append(bsoncxx::builder::basic::kvp("key1", "1"));
-
-     bsoncxx::builder::basic::document update_doc;
-     update_doc.append(bsoncxx::builder::basic::kvp("$set",bsoncxx::builder::stream::open_document<< "field_to_update" << "new_value" <<bsoncxx::builder::stream::close_document);
-
-     connector.UpdateDocument("mydb", "mycollection", filter.extract(), update_doc.extract());
-
-
-
-     // Пример удаления документа
-     bsoncxx::builder::basic::document filter_builder;
-     filter_builder.append(bsoncxx::builder::basic::kvp("LEv", "lev"));
-     connector.DeleteDocument("mydb", "mycollection", filter_builder.view());
-     */
-    return 0;
+    // Проверяем, что база данных и коллекция были созданы успешно
+    // Можно использовать другие методы MongoDBConnector для этой проверки
 }
+
+
+
+TEST_F(MongoDBConnectorTest, Drop) {
+    const char* database_name = "test_db1";
+
+    
+    connector.Drop(database_name);
+
+    // Проверяем, что база данных была удалена успешно
+    // Можно использовать другие методы MongoDBConnector для этой проверки
+}
+
+
+TEST_F(MongoDBConnectorTest, InsertDocument) {
+    const char* database_name = "test_db2";
+    const char* collection_name = "test_collection";
+    //bsoncxx::document::value doc_value = bsoncxx::from_json("{\"key\": \"value\"}");
+
+    std::string jsonContent;
+    std::ifstream jsonFile("data_model.json");
+    if (jsonFile.is_open()) {
+        std::getline(jsonFile, jsonContent, '\0');
+        jsonFile.close();
+    }
+    else {
+        std::cerr << "Unable to open JSON file." << std::endl;
+        return;
+    }
+
+    // Разбираем JSON строку
+    auto doc = bsoncxx::from_json(jsonContent);
+
+
+    connector.InsertDocument(database_name, collection_name, doc);
+
+    // Проверяем, что документ был успешно вставлен
+    // Можно использовать другие методы MongoDBConnector для этой проверки
+}
+
+
+TEST_F(MongoDBConnectorTest, UpdateDocument) {
+    const char* database_name = "test_db2";
+    const char* collection_name = "test_collection";
+
+    bsoncxx::builder::stream::document filter{};
+    filter << "0.nfType" << "NRF";
+
+    bsoncxx::builder::stream::document update{};
+    update << "$set" << bsoncxx::builder::stream::open_document << "0.nfInstanceId" << "01234" << bsoncxx::builder::stream::close_document;
+
+    connector.UpdateDocument(database_name, collection_name, filter.view(), update.view());
+
+}
+
+
+TEST_F(MongoDBConnectorTest, DeleteDocument) {
+    const char* database_name = "test_db2";
+    const char* collection_name = "test_collection";
+
+    bsoncxx::builder::stream::document filter{};
+    filter << "key" << "value";
+
+    bsoncxx::builder::stream::document update{};
+    update << "$set" << bsoncxx::builder::stream::open_document << "0.nfInstanceId" << "01234" << bsoncxx::builder::stream::close_document;
+
+    connector.DeleteDocument(database_name, collection_name, filter.view());
+
+}
+
+
+
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    mongocxx::instance instance{};
+    return RUN_ALL_TESTS();
+}
+
