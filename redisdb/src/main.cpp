69b920c366923ea30d4d6b560b9d7a60d983c2fa
Рябов Георгий Дмитриевич <gdryabov@edu.hse.ru>
2023-11-28 13:23:04 +0300

    update benchmark

--- redisdb/src/main.cpp
+++ redisdb/src/main.cpp
@@ -11,28 +11,7 @@ int main() {
     
     auto redis = std::make_unique<RedisInterface>("tcp://127.0.0.1:6379");
 
-    try {
-
-        // auto v = redis->find("NRF");
-
-        // for (const auto& i : v) {
-        //     std::cout << *i << " ";
-        //     auto str = redis->hget(*i, "data");
-        //     // std::cout << *str;
-        //     // json profile = json::parse(*str);
-        //     // std::cout << std::setw(4) << profile.flatten() << "\n\n";
-        // }
-
-        // redis->loadingDB("../data/data_model.json");
-
-        redis->createDB("../data/data_model.json", 3, "NFR");
-
-        // redis.flushdb();
-        // redis.flushdb();
-
-    } catch (const std::exception& e) {
-        std::cerr << "Ошибка: " << e.what() << std::endl;
-    }
+    std::cout << "Hello, World!";
 
     return 0;
 }
--- redisdb/src/redis_find.cpp
+++ redisdb/src/redis_find.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <random>
 
 #include <sw/redis++/redis++.h>
 #include <sw/redis++/redis.h>
@@ -30,19 +31,62 @@ bool RedisInterface::create(const StringView &key, const StringView &val) {
     return set(key, val);
 }
 
+// set method for hash, return bool.
+bool RedisInterface::hcreate(const StringView &key, 
+                             const StringView &field, 
+                             const StringView &dataField) {
+    (*this).hset(key, field, dataField);
+    return true;
+}
+
 // get method, return OptionalString.
 OptionalString RedisInterface::read(const StringView &key) {                
     return get(key);
 }        
+
+// get method for hash, return OptionalString.
+OptionalString RedisInterface::hread(const StringView &key, 
+                                     const StringView &field) {                
+    return (*this).hget(key, field);
+}        
+
+// update method, return long long.
+bool RedisInterface::update(const StringView &key, const StringView &val) {
+    if (!(*this).read(key)) {
+        (*this).create(key, val);
+        return true;
+    }
+    return false;
+}
+
+// update method for hash, return long long.
+bool RedisInterface::hupdate(const StringView &key, 
+                             const StringView &field, 
+                             const StringView &dataField) {
+    if (!(*this).hget(key, field)) {
+        (*this).hset(key, field, dataField);
+        return true;
+    }
+    return false;
+}
 
 // del method, return long long.
 long long RedisInterface::del(const StringView &key) {                     
     return Redis::del(key);
 }
 
+// del method for hash, return long long.
+long long RedisInterface::hdel(const StringView &key, const StringView &field) {                     
+    return Redis::hdel(key, field);
+}
+
+//===----------------------------------------------------------------------===//
+// general methods.
+//===----------------------------------------------------------------------===//
+
 // return the keys by search pattern.
 std::vector<OptionalString> RedisInterface::find(
-                                            const std::string nfTypeSearch) {
+                                            const StringView &nfTypeSearch) {
     std::vector<OptionalString> match_keys;
     std::vector<OptionalString> keys;
 
@@ -55,39 +99,30 @@ std::vector<OptionalString> RedisInterface::find(
     return match_keys;
 }
 
-// loading data bate from the json by the json path.
-void RedisInterface::loadingDB(const char* config_path) {
+// creating DB with uniform dist nfprofiles. 14064 byte or ~ 0,014 Mb per key.
+void RedisInterface::createUniDB(const char* config_path, const int n) {
     std::ifstream cf(config_path); 
 
     if (cf.is_open()) {
-        json instances = json::parse(cf);  
+        std::vector<std::string> nfTypes {"NRF", "UDM", "AMF", "SMF", "AUSF", 
+                                          "NEF", "PCF", "SMSF", "NSSF", "UDR", 
+                                          "LMF", "GMLC", "5G_EIR", "SEPP", "UPF"
+                                          "N3IWF", "AF", "UDSF", "BSF", "CHF", 
+                                          "NWDAF"}; // 20 nftypes.
 
-        for (auto& profile : instances) {      
-            std::string nfInstanceId = profile["nfInstanceId"];
-            std::string nfType = profile["nfType"];
-            (*this).hset(nfInstanceId, "nfType", nfType);
-            (*this).hset(nfInstanceId, "data", profile.dump(4));
-        }
-
-        cf.close();   
-    }
-}
-
-// creating nfprofiles. 14064 byte or ~ 0,014 Mb per key.
-void RedisInterface::createDB(const char* config_path, 
-                              int n, 
-                              std::string nfType) {
-    std::ifstream cf(config_path); 
-
-    if (cf.is_open()) {
+        std::mt19937 rng;
+        const unsigned int seed = 42;
+        rng.seed(seed);
+        std::uniform_int_distribution<int> dist(0, 20);
         json instances = json::parse(cf);  
 
         for (auto& profile : instances) {  // for 1 of 1. 
             for (int i = 0; i < n; ++i) {
                 const std::string nfInstanceId = std::to_string(i);
+                const int randomNumber = dist(rng);
                 profile["nfInstanceId"] = nfInstanceId;
-                profile["nfType"] = nfType;
-                (*this).hset(nfInstanceId, "nfType", nfType);
+                profile["nfType"] = nfTypes[randomNumber];
+                (*this).hset(nfInstanceId, "nfType", nfTypes[randomNumber]);
                 (*this).hset(nfInstanceId, "data", profile.dump());
             }
         }
