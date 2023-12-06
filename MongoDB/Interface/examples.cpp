34c1757dad7f0b71d9a81c1dcaea45866ee2ab31
LevCov <100800333+LevCov@users.noreply.github.com>
2023-12-06 14:44:06 +0300

    Update examples.cpp
    Signed-off-by: LevCov <100800333+LevCov@users.noreply.github.com>

--- MongoDB/Interface/examples.cpp
+++ MongoDB/Interface/examples.cpp
@@ -18,18 +18,38 @@ MongoDBConnector connector{ "mongodb://localhost:27017" };
 const char* database_name = "DBName";
 const char* collection_name = "ColName";
 
-/-------------------------------------------------/
+//-------------------------------------------------/
 //Пример как пользоваться методом Create()  
   
 connector.Create(database_name1, collection_name);
   
-/-------------------------------------------------/
-  //Пример как пользоваться методом Drop()
+//-------------------------------------------------/
+//Пример как пользоваться методом Drop()
 
-  connector.Drop(database_name);
+connector.Drop(database_name);
 
-/-------------------------------------------------/
+//-------------------------------------------------/
+//Пример как пользоваться методом InsertDocument();
 
+
+//c  
+std::string jsonContent;
+std::ifstream jsonFile("data_model.json");
+if (jsonFile.is_open())
+{
+    std::getline(jsonFile, jsonContent, '\0');
+    jsonFile.close();
+}
+else
+{
+    std::cerr << "Unable to open JSON file." << std::endl;
+    return;
+}
+
+    // Разбираем JSON строку
+    auto doc = bsoncxx::from_json(jsonContent);
+
+  
 
 /-------------------------------------------------/
 /-------------------------------------------------/
