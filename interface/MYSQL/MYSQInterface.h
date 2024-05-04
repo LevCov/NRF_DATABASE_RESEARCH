371050afea9bdbc0910aca6a13736372b3d46877
LevCov <100800333+LevCov@users.noreply.github.com>
2024-05-04 19:31:10 +0300

    Update MYSQInterface.h
    Signed-off-by: LevCov <100800333+LevCov@users.noreply.github.com>

--- interface/MYSQL/MYSQInterface.h
+++ interface/MYSQL/MYSQInterface.h
@@ -1,4 +1,9 @@
-#include "MYSQLInterface.h"
+
+#include <iostream> 
+#include <mysql_connection.h> 
+#include <mysql_driver.h> 
+#include <cppconn/statement.h> 
+#include <cppconn/prepared_statement.h> 
 
 class MYSQLInterface
 {
