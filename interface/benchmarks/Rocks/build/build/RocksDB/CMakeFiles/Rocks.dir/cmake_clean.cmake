580693a3178110f44815e37538c89c1fc2d3304f
Vitaliy Tsyvunin <vcyvunin@gmail.com>
2024-05-04 19:01:41 +0300

    Modify repo

---
+++ interface/benchmarks/Rocks/build/build/RocksDB/CMakeFiles/Rocks.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rocks.dir/RocksDBInterface.cpp.o"
+  "CMakeFiles/Rocks.dir/RocksDBInterface.cpp.o.d"
+  "libRocks.a"
+  "libRocks.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rocks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
